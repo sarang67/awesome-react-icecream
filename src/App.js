8f95f9c1e226d52f4e1d0d8342649bb400e86466
sarang <sarang>
2022-02-26 15:13:39 +0530

    1) Start boilerplate for application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/ice-cream.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Your Awesome Application goes there.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
